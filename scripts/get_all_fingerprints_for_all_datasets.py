1af62dcff541233abe39ca422ec91bbd2e389191
George Batchkala <george.batchkala@gmail.com>
2020-07-13 15:58:28 +0300

    create all ecfp with: nBits = 1024, 2048; diameter = 4, 6
    record time

--- scripts/get_all_fingerprints_for_all_datasets.py
+++ scripts/get_all_fingerprints_for_all_datasets.py
@@ -1,10 +1,13 @@
 # Import modules
+import time
 import numpy as np
 import pandas as pd
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+# record time to show after execution of the script
+since = time.time()
 
 # For each dataset*smile_type combination get the ECFP-4 and ECFP-6 features from smile strings
 for dataset in ['esol', 'freesolv', 'lipophilicity']:
@@ -21,17 +24,22 @@ for dataset in ['esol', 'freesolv', 'lipophilicity']:
 
         # get ecfp-4, ecfp-6 (with radii respectively 2 and 3)
         for radius in [2, 3]:
-            # get ECFP features
-            ecfp = [AllChem.GetMorganFingerprintAsBitVect(m, radius=radius, nBits=2048) for m in ms]
-            ecfp_np = np.array(ecfp)
-            ecfp_pd = pd.DataFrame(ecfp_np)
-            ecfp_pd.index = list(data.index)
-            ecfp_pd.columns = [f'ecfp{radius*2}-{i}' for i in range(len(ecfp_pd.columns))]
+            # get them in 1024 and 2048 bit versions
+            for nBits in [1024, 2048]:
+                # get ECFP features
+                ecfp = [AllChem.GetMorganFingerprintAsBitVect(m, radius=radius, nBits=nBits) for m in ms]
+                ecfp_np = np.array(ecfp)
+                ecfp_pd = pd.DataFrame(ecfp_np)
+                ecfp_pd.index = list(data.index)
+                ecfp_pd.columns = [f'{nBits}ecfp{radius*2}-{i}' for i in range(len(ecfp_pd.columns))]
 
-            # save ECFP features to .csv files
-            file_path = f'../data/{dataset}_{smile_type}_ecfp{radius*2}_features.csv'
-            ecfp_pd.to_csv(file_path, index=True)
-            print(f'Saved file to: {file_path}')
+                # save ECFP features to .csv files
+                file_path = f'../data/{dataset}_{smile_type}_{nBits}ecfp{radius*2}_features.csv'
+                ecfp_pd.to_csv(file_path, index=True)
+                print(f'Saved file to: {file_path}')
 
         # skip line when starting to work on a new dataset*smile_type combination
         print()
+
+time_elapsed = time.time() - since
+print(f'Task completed in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s \n')

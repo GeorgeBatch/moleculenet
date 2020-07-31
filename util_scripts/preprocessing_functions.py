9ae4e7b33a398fa4a645295741881401067828ab
George Batchkala <george.batchkala@gmail.com>
2020-07-31 10:24:59 +0300

    create a script with utility functions. For now, with list_highly_correlated function

---
+++ util_scripts/preprocessing_functions.py
@@ -0,0 +1,44 @@
+# standard imports
+import numpy as np
+import pandas as pd
+
+
+def list_highly_correlated(df_features, targets, threshold=0.8):
+    """
+    List column names of the dataframe of features which are highly correlated
+    to the target (absolute value of the correlation is greater than threshold).
+
+    df_features: pandas.core.frame.DataFrame of features
+    targets: pandas.core.series.Series of targets
+    threshold: float in [0, 1] above which we consider a feature highly correlated
+
+    returns: list of columns names from df_features, which are highly correlated
+             to the target
+
+    """
+    # check bounds for abs(correlation) threshold
+    assert 0 <= threshold <= 1
+
+    # df_features and targets should have the same length
+    assert df_features.shape[0] == targets.shape[0]
+    print('Original shapes:                 ', df_features.shape, targets.shape)
+
+    # remove na rows
+    X = df_features.dropna(axis=0)
+    y = targets[X.index]
+    print('Removed NA rows, shapes:         ', X.shape, y.shape)
+
+    # remove zero-variace columns
+    zero_std = X.std() < 1e-5
+    zero_std_cols = X.columns[zero_std]
+    X = X.drop(zero_std_cols, axis=1)
+    print('Removed zero-var columns, shapes:', X.shape, y.shape)
+
+    # record highly correlated features
+    cols_to_remove = []
+    for name in X.columns:
+        # print(name, np.abs(pearsonr(X[name], y)[0]))
+        if np.abs(pearsonr(X[name], y)[0]) > threshold:
+            cols_to_remove.append(name)
+
+    return cols_to_remove

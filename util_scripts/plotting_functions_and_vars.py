f28cbabe0c67232b66785af1dd9ad850fb5baacd
George Batchkala <george.batchkala@gmail.com>
2020-08-16 14:33:40 +0300

    add plotting fcts script to standardie plotting

---
+++ util_scripts/plotting_functions_and_vars.py
@@ -0,0 +1,20 @@
+# Standard imports
+import sys
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# -----------------------------------------------------------------------------
+# Global variables to be used in scripts and notebooks
+
+
+# -----------------------------------------------------------------------------
+# Global plotting options
+
+FIGSIZE = (6, 6)
+
+PLOTS_DIR = '../plots'
+
+# -----------------------------------------------------------------------------
+# Plotting functions

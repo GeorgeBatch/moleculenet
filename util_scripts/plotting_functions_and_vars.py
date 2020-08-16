7c3c66fb7d17a0b0e54f009fb837a08ce6178d2c
George Batchkala <george.batchkala@gmail.com>
2020-08-16 15:28:51 +0300

    add mappings: datasets to units, datasets to titles, metrics to labels
    change plots directory to '../figures/'

--- util_scripts/plotting_functions_and_vars.py
+++ util_scripts/plotting_functions_and_vars.py
@@ -8,13 +8,34 @@ import matplotlib.pyplot as plt
 # -----------------------------------------------------------------------------
 # Global variables to be used in scripts and notebooks
 
+# titles (everywhere)
+datasets_to_titles = {
+    'freesolv': 'solvation energy'.title(),
+    'esol': 'log solubility'.title(),
+    'lipophilicity': 'octanol/water distribution coefficient'.title()
+}
+
+# units (CIs)
+datasets_to_units = {
+    'freesolv': '(kcal/mol)',
+    'esol': '(mol/litre)',
+    'lipophilicity': '(logD)'
+}
+
+# labels (heatmaps)
+metrics_to_labels = {
+    'RMSE': 'root-mean-square error',
+    'MAE': 'mean absolute error',
+    'R^2': 'R^2 (coefficient of determination)',
+    'pearson_r': 'Pearson correlation'
+}
 
 # -----------------------------------------------------------------------------
 # Global plotting options
 
 FIGSIZE = (6, 6)
 
-PLOTS_DIR = '../plots'
+PLOTS_DIR = '../figures'
 
 # -----------------------------------------------------------------------------
 # Plotting functions
